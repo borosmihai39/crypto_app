77e2d7585e3745d7b810815c730c170d205a393f
Mihai Boros <57368282+borosmihai39@users.noreply.github.com>
2022-05-12 10:40:50 +0300

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const cryptoApiHeaders = {
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-  "X-RapidAPI-Key": "4e06ad461dmsh5bd215cbacddc9dp105563jsn56d14289e6ae",
+  "X-RapidAPI-Key": "...",
 };
 const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
 
